1524aebe763de67f9f8f1cf63879cdfef049dff2
varshithpbs007 <150416730+varshithpbs007@users.noreply.github.com>
2026-03-13 10:16:37 +0530

    Update 01. Single Linked list with user selected operations.c

--- Linked Lists/01. Single Linked list with user selected operations.c
+++ Linked Lists/01. Single Linked list with user selected operations.c
@@ -1,144 +1,157 @@
-/* Singly Linked List Implementation in C */
-/*Including the necessary header files*/
-#include<stdio.h> // contains printf() and scanf()
-#include<stdlib.h> //contains malloc()
+/* Implementation of Single Linked List in C */
 
-/* Globally struct data type node is created which is  accessed by all methods */
-struct node
-{
+#include<stdio.h> // This library contains standard input (scanf()), and output (printf()) functions
+#include<stdlib.h> // This library contains malloc() function
+
+// Functions prototypes other than main()
+void append(void); // 1
+void addatbeginning(void); // 2
+void addafter(void); // 3
+int length(void); // 4
+void display(void); // 5
+void deletenode(void); // 6
+
+struct node // Global struct type variable(user defined) named node , accessed by all the functions
+{ 
     int data;
-    struct node *link;
+    struct node* link;
 };
 
-struct node *root = NULL;//global variable is by default initiated with NULL
-int len;//global variable to save the length of linked list
+struct node *root = NULL; // Global varibales (here root) are by default initialized with 0, even when un-innitialised
+int len; // Global varibales are by default initialized with 0
 
-/* Function prototypes */
-/*1*/ void append(void);
-/*2*/ void addatbegin(void);
-/*3*/ void addafter(void);
-/*4*/ int length(void);
-/*5*/ void display_list(void);
-/*6*/ void delete_node(void);
-
-/*main() function from where the execution is started*/
-void main()
+void  main()
 {
-    int choice;//local variable to main() to read the user's choice into
+    int choice; // local variable as it is only used by main() function
     while(1)
     {
-        printf("Single Liked List Operations:\n");
-        printf("1. Append\n");
-        printf("2. Add at begin\n");
-        printf("3. Add after\n");
-        printf("4. Length\n");
-        printf("5. Display list\n");
-        printf("6. Delete\n");
-        printf("7. Quit\n");
-        
-        printf("Enter your choice of operation:\n");
+        printf("Choose from the below available Single Linked List Operations:\n");
+
+        printf("1.Append (Addatend)\n");
+        printf("2.Addatbeginning\n");
+        printf("3.Addafter\n");
+        printf("4.length\n");
+        printf("5.Display\n");
+        printf("6.Deletenode\n");
+        printf("7.Quit\n");
+
+        printf("Enter your choice\n");
         scanf("%d",&choice);
-        
-        switch (choice)
+
+        switch(choice)
         {
-            case 1 : append();
-                     break;
-            case 2 : addatbegin();
-                     break;
-            case 3 : addafter();
-                     break;
-            case 4 : len = length();
-                     printf("Length of list is %d nodes\n",len);
-                     break;
-            case 5 : display_list();
-                     break;
-            case 6 : delete_node();
-                     break;
-            case 7 : exit(1);
-            default : printf("Invalid choice number\n");
+            case 1: append();
+                    break;
+            case 2: addatbeginning();
+                    break;
+            case 3: addafter();
+                    break;
+            case 4: len = length();
+                    printf("Length of Linked list is %d\n",len);
+                    break;
+            case 5: display();
+                    break;
+            case 6: deletenode();
+                    break;
+            case 7: exit(1);
+
+            default: printf("INVALID choice\n");
         }
-        
     }
+
+
 }
 
-/* Writing method defenitions for all the choices */
 
-//1. Append (adding a node at the last)
+// Custom functions to achieve linked list operations
+
+// 1
 void append()
 {
-    struct node *temp; //local varible --> gets automatically deleted after every call
-    temp = (struct node*)malloc(sizeof(struct node)); //creation of a node and saving its base address in temp
-    printf("Enter node data : \n");//asking the user what data to read
-    scanf("%d", &temp -> data);//reading the user entered data into data field pointed by temp
-    temp -> link = NULL;//always keep the newly created link field NULL initially
+    struct node* temp;
+    temp = (struct node*)malloc(sizeof(struct node));
     
-    if(root == NULL) //if list is empty is checked
+    printf("Enter data to save in node:\n");
+    scanf("%d",&temp->data);
+    temp->link = NULL;
+    // till now node is created and temp is pointing to created node
+
+    if(root == NULL) // i.e., the list is empty
     {
-        root = temp;//list empty means the created node is 1st node and root should point where temp is pointing
+        root = temp; // assigning temp contents to root so that the root points to created node
     }
     else
     {
-        struct node *p;//local variable created to perform traversal to check which link is NULL so that we can append the new node there
-        p = root;//root is passed and further temporary ops are done on p
-        while(p -> link != NULL)
+        struct node* p;
+        p = root;
+        while(p->link != NULL)
         {
-            p = p -> link; //p gets the link address to point to next node and again check in the loop that link is NULL or not till last position
+            p = p->link;
         }
-        p -> link = temp; //this line appends the node at last position
+        p->link = temp; // this line appends the new node at the end of the list
+    
     }
-}
-
-//2. Add at begin
-void addatbegin(void)
-{
     
 }
 
-//3, Add after
+
+//2
+void addatbeginning(void)
+{
+
+}
+
+
+//3
 void addafter(void)
 {
-    
+
 }
 
-//4. Length of list
-int length()
+
+//4
+int length(void)
 {
     int count = 0;
-    struct node *temp;
+    struct node* temp;
     temp = root;
-    
+
     while(temp != NULL)
     {
         count++;
-        temp = temp -> link;
+        temp = temp->link;
     }
+
     return count;
-    
+
 }
 
-//5. Displaying all the elements in the list
-void display_list()
+
+//5
+void display(void)
 {
-    struct node *temp;
+    struct node* temp;
     temp = root;
+
     if(temp == NULL)
     {
-         printf("No elements in the list\n\n");
+        printf("List is EMPTY");
     }
     else
     {
         while(temp != NULL)
         {
-            printf("%d -->",temp -> data);
-            temp = temp -> link;
+            printf("%d->",temp->data);
+            temp = temp->link;
         }
+
         printf("\n\n");
     }
-   
+
 }
 
-//6. Delete a node from list
-void delete_node(void)
+//6
+void deletenode(void)
 {
     
 }
